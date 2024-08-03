29a3a81a486f3bdc3b851163a58070b3637f9e24
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2024-08-03 21:13:50 +0530

    frist commit

---
+++ src/main/java/com/adminecom/adminecom/AdminecomApplication.java
@@ -0,0 +1,13 @@
+package com.adminecom.adminecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminecomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminecomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adminecom/adminecom/AdminecomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adminecom.adminecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminecomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
